7a7bf42e5e1238f29a8e2f5286053e7c36fcfe36
Keith-appdev <mabaylan.187237@rosario.sti.edu.ph>
2024-12-20 09:51:14 +0800

    Added Connectors within Pages

--- InventorySystem/src/GUI/Login.java
+++ InventorySystem/src/GUI/Login.java
@@ -6,6 +6,7 @@ import com.formdev.flatlaf.FlatLightLaf;
  *
  * @author Adrian
  */
+import javax.swing.*;
 
 public class Login extends javax.swing.JFrame {
 
@@ -33,7 +34,7 @@ public class Login extends javax.swing.JFrame {
         labPass = new javax.swing.JLabel();
         labLog = new javax.swing.JLabel();
         buttOk = new javax.swing.JButton();
-        buttReg = new javax.swing.JButton();
+        BtRegister = new javax.swing.JButton();
         jCheckBox1 = new javax.swing.JCheckBox();
         pfPass = new javax.swing.JPasswordField();
         jPanel2 = new javax.swing.JPanel();
@@ -51,25 +52,19 @@ public class Login extends javax.swing.JFrame {
         icon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/inventory1.png"))); // NOI18N
         icon.setOpaque(true);
 
-        tfUser.setBackground(new java.awt.Color(255, 255, 255));
         tfUser.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
-        tfUser.setForeground(new java.awt.Color(0, 0, 0));
 
         labUser.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
-        labUser.setForeground(new java.awt.Color(0, 0, 0));
         labUser.setLabelFor(tfUser);
         labUser.setText("USERNAME");
 
         labPass.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
-        labPass.setForeground(new java.awt.Color(0, 0, 0));
         labPass.setText("PASSWORD");
 
         labLog.setBackground(new java.awt.Color(255, 255, 255));
         labLog.setFont(new java.awt.Font("Segoe UI Semibold", 0, 24)); // NOI18N
-        labLog.setForeground(new java.awt.Color(0, 0, 0));
         labLog.setText("Login");
 
-        buttOk.setBackground(new java.awt.Color(255, 255, 255));
         buttOk.setForeground(new java.awt.Color(255, 255, 255));
         buttOk.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/arr.png"))); // NOI18N
         buttOk.setOpaque(true);
@@ -79,19 +74,19 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
-        buttReg.setBackground(new java.awt.Color(255, 255, 255));
-        buttReg.setFont(new java.awt.Font("Segoe UI Semibold", 0, 14)); // NOI18N
-        buttReg.setForeground(new java.awt.Color(0, 0, 0));
-        buttReg.setText("Register");
-        buttReg.setOpaque(true);
+        BtRegister.setFont(new java.awt.Font("Segoe UI Semibold", 0, 14)); // NOI18N
+        BtRegister.setText("Register");
+        BtRegister.setOpaque(true);
+        BtRegister.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtRegisterActionPerformed(evt);
+            }
+        });
 
         jCheckBox1.setBackground(new java.awt.Color(255, 255, 255));
-        jCheckBox1.setForeground(new java.awt.Color(0, 0, 0));
         jCheckBox1.setText("Remember Me");
 
-        pfPass.setBackground(new java.awt.Color(255, 255, 255));
         pfPass.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
-        pfPass.setForeground(new java.awt.Color(0, 0, 0));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -119,7 +114,7 @@ public class Login extends javax.swing.JFrame {
                         .addComponent(buttOk))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(122, 122, 122)
-                        .addComponent(buttReg, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(BtRegister, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -142,7 +137,7 @@ public class Login extends javax.swing.JFrame {
                 .addGap(120, 120, 120)
                 .addComponent(buttOk)
                 .addGap(62, 62, 62)
-                .addComponent(buttReg)
+                .addComponent(BtRegister)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
@@ -190,7 +185,21 @@ public class Login extends javax.swing.JFrame {
 
     private void buttOkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttOkActionPerformed
         // TODO add your handling code here:
+
+    
+       
     }//GEN-LAST:event_buttOkActionPerformed
+
+    private void BtRegisterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtRegisterActionPerformed
+        // TODO add your handling code here:
+           
+       // Clear the current content pane and replace it with the registration panel
+    this.getContentPane().removeAll(); // Remove all existing components
+    Register regPanel = new Register(); // Create an instance of the Register panel
+    this.setContentPane(regPanel); // Set the Register panel as the content pane
+    this.revalidate(); // Refresh the frame
+    this.repaint(); // Redraw the frame
+    }//GEN-LAST:event_BtRegisterActionPerformed
 
     /**
      * @param args the command line arguments
@@ -221,6 +230,10 @@ public class Login extends javax.swing.JFrame {
         //</editor-fold>
         FlatLightLaf.setup();
         /* Create and display the form */
+               Register reg = new Register();
+       Login log = new Login();
+       reg.setBounds(50, 50, 700, 500); // Set the size and position of the panel
+       log.add(reg);
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Login().setVisible(true);
@@ -229,8 +242,8 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtRegister;
     private javax.swing.JButton buttOk;
-    private javax.swing.JButton buttReg;
     private javax.swing.JLabel icon;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
